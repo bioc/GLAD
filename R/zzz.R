9a7bb760348e626d02cbd549b2d82ef174154d1c
Martin Morgan <mtmorgan@fhcrc.org>
2016-04-29 21:47:02 +0000

    place startup message in .onAttach
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GLAD@116930 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,26 +1,27 @@
-.onLoad <- function(lib, pkg){
+.onAttach <- function(lib, pkg){
 
-  packageStartupMessage("\n")
-  packageStartupMessage("######################################################################################\n")
-  packageStartupMessage("\n")
-  packageStartupMessage("Have fun with GLAD\n")
-  packageStartupMessage("\n")  
-  packageStartupMessage("For smoothing it is possible to use either\n")
-  packageStartupMessage("the AWS algorithm (Polzehl and Spokoiny, 2002)\n")
-  packageStartupMessage("or the HaarSeg algorithm (Ben-Yaacov and Eldar, Bioinformatics,  2008)\n")
-  packageStartupMessage("\n")
-  packageStartupMessage("If you use the package with AWS, please cite:\n")
-  packageStartupMessage("Hupe et al. (Bioinformatics, 2004) and Polzehl and Spokoiny (2002)\n")
-  packageStartupMessage("\n")    
-  packageStartupMessage("If you use the package with HaarSeg, please cite:\n")
-  packageStartupMessage("Hupe et al. (Bioinformatics, 2004) and (Ben-Yaacov and Eldar, Bioinformatics, 2008)\n")
-  packageStartupMessage("\n")
-  packageStartupMessage("For fast computation it is recommanded to use\n")
-  packageStartupMessage("the daglad function with smoothfunc=haarseg\n")
-  packageStartupMessage("\n")
-  packageStartupMessage("######################################################################################\n")    
-  packageStartupMessage("\n")
-  packageStartupMessage("New options are available in daglad: see help for details.")
-  packageStartupMessage("\n")  
-    
+  packageStartupMessage(
+      "\n",
+      "######################################################################################\n",
+      "\n",
+      "Have fun with GLAD\n",
+      "\n",  
+      "For smoothing it is possible to use either\n",
+      "the AWS algorithm (Polzehl and Spokoiny, 2002,\n",
+      "or the HaarSeg algorithm (Ben-Yaacov and Eldar, Bioinformatics,  2008,\n",
+      "\n",
+      "If you use the package with AWS, please cite:\n",
+      "Hupe et al. (Bioinformatics, 2004, and Polzehl and Spokoiny (2002,\n",
+      "\n",    
+      "If you use the package with HaarSeg, please cite:\n",
+      "Hupe et al. (Bioinformatics, 2004, and (Ben-Yaacov and Eldar, Bioinformatics, 2008,\n",
+      "\n",
+      "For fast computation it is recommanded to use\n",
+      "the daglad function with smoothfunc=haarseg\n",
+      "\n",
+      "######################################################################################\n",    
+      "\n",
+      "New options are available in daglad: see help for details.",
+      "\n")
+
 }
